a75747d0179e3135ebf0e1a4ae751d6b4aedfd51
MatthewLC <mateusluiztj@hotmail.com>
2020-07-20 21:51:33 -0300

    Adding Test Project

---
+++ ShellPresentation.UITests/AppInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace ShellPresentation.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                //return ConfigureApp.Android.StartApp();
+                return ConfigureApp.Android.InstalledApp("com.companyname.shellpresentation").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
